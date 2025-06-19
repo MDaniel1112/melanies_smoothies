f59a4d2257a0bd8da3aab0c2145cd7c7c59efb97
MDaniel1112 <m.ishal@hotmail.com>
2025-06-19 11:58:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.functions import col, when_matched
+#from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize your Smoothie!  :cup_with_straw: ")
